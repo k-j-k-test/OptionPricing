759fb64343461d64ee69e898aec7b9d82d88f548
k-j-k-test <158127335+k-j-k-test@users.noreply.github.com>
2025-03-15 04:03:53 +0900

    c2

---
+++ js/app.js
@@ -0,0 +1,10 @@
+﻿// 파일 저장 기능
+window.saveAsFile = function (filename, byteBase64) {
+    // Base64 문자열을 바이너리로 변환
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + byteBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
